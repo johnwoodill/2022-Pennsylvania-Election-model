0ebfc3fcebd442fdd344b616867833634b77a812
johnwoodill <johnwoodill@gmail.com>
2022-10-21 13:55:53 -0700

    finished historical fn

--- scrape_data.py
+++ scrape_data.py
@@ -241,6 +241,145 @@ def get_county_wide(candidates):
 
 
 
+
+year = 2012
+year = 2020
+
+def get_county_wide(year):
+    type_lookup = {'2022': 'undefined', '2020': '83', '2018': '63', '2016': '54', '2014': '41', '2012': '27',
+        '2010': '19', '2008': '17', '2006': '15', '2004': '13', '2002': '11', '2000': '9'}
+    
+    ElectionID = type_lookup[str(year)]
+    ElectionType = 'G'
+
+    url = f"https://www.electionreturns.pa.gov/General/SummaryResults?ElectionID={ElectionID}&ElectionType={ElectionType}&IsActive=0"
+
+    # Setup s3fs
+    s3 = s3fs.S3FileSystem(anon=False, key=ACCESS_ID, secret=SECRET)
+
+    # Setup browser to process javascript
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+
+    pa_counties = ['Adams', 'Allegheny', 'Armstrong', 'Beaver', 'Bedford', 'Berks', 'Blair', 
+        'Bradford', 'Bucks', 'Butler', 'Cambria', 'Cameron', 'Carbon', 'Centre', 'Chester', 
+        'Clarion', 'Clearfield', 'Clinton', 'Columbia', 'Crawford', 'Cumberland', 'Dauphin', 
+        'Delaware', 'Elk', 'Erie', 'Fayette', 'Forest', 'Franklin', 'Fulton', 'Greene', 
+        'Huntingdon', 'Indiana', 'Jefferson', 'Juniata', 'Lackawanna', 'Lancaster', 'Lawrence', 
+        'Lebanon', 'Lehigh', 'Luzerne', 'Lycoming', 'McKEAN', 'Mercer', 'Mifflin', 'Monroe', 
+        'Montgomery', 'Montour', 'Northampton', 'Northumberland', 'Perry', 'Philadelphia', 
+        'Pike', 'Potter', 'Schuylkill', 'Snyder', 'Somerset', 'Sullivan', 'Susquehanna', 'Tioga', 
+        'Union', 'Venango', 'Warren', 'Washington', 'Wayne', 'Westmoreland', 'Wyoming', 'York']
+
+    # Loop through counties and save
+    county_lst = []
+    for county_ in pa_counties:
+
+        # Set url to parse
+        url = f"https://www.electionreturns.pa.gov/General/CountyResults?countyName={county_}&ElectionID={ElectionID}&ElectionType={ElectionType}&IsActive=0"
+
+        # Setup browser
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.get(url) 
+        
+        # Wait for page to load then scrape data
+        time.sleep(1)
+        text = driver.find_element(By.CSS_SELECTOR, "div.panel-body").get_attribute("innerText")
+        split_text = text.split("\n")
+
+        # Get list of all races
+        panels = [i for i, j in enumerate(split_text) if j == 'Back to Top']
+        races = [i for i, j in enumerate(split_text) if j == 'Filter Options']
+    
+        # Loop through Filter options and stop when repeated        
+        lst_ = []
+        for race_ in np.arange(20):
+            lst_.append(split_text[race_])
+            if any(lst_.count(x) > 1 for x in lst_):
+                lst_ = lst_[1:-1]
+                break
+
+        # Get individual panel indexes
+        race_panels_ = []
+        for i, race_ in enumerate(lst_):
+            race_ = lst_[i]
+            start_panel = [i for i, j in enumerate(split_text) if j == race_]  
+            start_panel = start_panel[1]              
+            end_panel = panels[i]
+            outdat = pd.DataFrame({'race': [race_], 'start_idx': [start_panel], 'end_idx': [end_panel]})
+            race_panels_.append(outdat)
+
+        # Concat data and filter
+        race_data = pd.concat(race_panels_)
+
+        race_data = race_data[race_data['race'] \
+            .isin(['President of the United States', 
+                   'United States Senator', 'Governor'])]
+
+        race_data = race_data.reset_index(drop=True)
+
+        # Loop through each race and get data
+        race_lst = []  
+        for i in np.arange(len(race_data)):
+            race = race_data.loc[i, 'race']
+            start_panel = race_data.loc[i, 'start_idx']
+            end_panel = race_data.loc[i, 'end_idx']
+
+            panel_text = split_text[start_panel:end_panel][1:]
+
+            # Set index based on changes in scrape
+            if any("Runningmate" in s for s in panel_text):
+                if any("Election" in s for s in panel_text):
+                    idx_ = 8
+                elif not any("Election" in s for s in panel_text):
+                    idx_ = 5
+            elif any("Election" in s for s in panel_text):
+                if not any("Runningmate" in s for s in panel_text):
+                    idx_ = 7
+            else:
+                idx_ = 4
+
+            # Number of panel splits
+            splits = int(len(panel_text)/idx_)
+          
+            # Loop through splits and collect data
+            for split_ in np.arange(splits):
+                # Separate panel
+                panel_split_text = panel_text[split_*idx_:(split_*idx_ + idx_)]
+                
+                # Get data
+                candidate = panel_split_text[0].split(",")[0].title()
+                party = panel_split_text[1]
+                votes = int(panel_split_text[3].split("\xa0")[-1].replace(",", ""))
+                
+                # Build df and append to list
+                indat = pd.DataFrame({'year': year, 'county': county_, 
+                            'race': race, 
+                            'candidate': [candidate], 
+                            'party': party,
+                            'votes': votes})
+                race_lst.append(indat)
+
+        # Merge data and get percentages
+        outdat = pd.concat(race_lst).reset_index(drop=True)
+        outdat = outdat.assign(perc_count = np.round( (outdat['votes'] / np.sum(outdat['votes'])) * 100, 2))
+        county_lst.append(outdat)
+        print(f"{year}: {county_}")
+        
+    # Concat and return
+    rdat = pd.concat(county_lst).reset_index(drop=True)
+    return rdat
+
+
+
+
+
+
+
+
+
 pa_gov = ["SHAPIRO, JOSHUA D", "MASTRIANO, DOUGLAS V", "HACKENBURG, JONATHAN MATTHEW", 
           "DIGIULIO, CHRISTINA PK", "SOLOSKI, JOSEPH P"]
 
